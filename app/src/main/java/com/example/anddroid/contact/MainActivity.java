a02a530d5717d4fc08aea5a6bfc5feab7d0966d4
DESKTOP-E9LVFLM\anddroid <abrarahmad3361@gmail.com>
2018-06-21 16:47:24 +0500

    Initial commit

---
+++ app/src/main/java/com/example/anddroid/contact/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.anddroid.contact;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.android.volley.*;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.microsoft.identity.client.*;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
